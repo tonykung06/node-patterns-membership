b17479870a374f16ce4cf0e523e3b9512373b336
tonykung <tonykung06@gmail.com>
2017-05-21 18:22:00 +0800

    initial commit

---
+++ gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = grunt => {
+	grunt.initConfig({
+		jshint: {
+			files: ['lib/**/*.js', 'models/**/*.js']
+		},
+		watch: {
+			files: ['lib/**/*.js', 'models/**/*.js'],
+			tasks: ['jshint']
+		}
+	})
+	grunt.loadNpmTasks('grunt-contrib-jshint')
+	grunt.loadNpmTasks('grunt-contrib-watch')
+}
---
+++ test/sample.js
@@ -0,0 +1,8 @@
+const assert = require('assert')
+describe('something', () => {
+	describe('nested thing', () => {
+		it('is good', () => {
+			assert.ok(true)
+		})
+	})
+})
